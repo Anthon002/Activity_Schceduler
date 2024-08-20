61e93ef125689b356c9b0745362fae2728880f97
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-20 08:22:03 +0100

    Update AuthentiationController.cs

--- Activity_Scheduler.MVC/Controllers/AuthentiationController.cs
+++ Activity_Scheduler.MVC/Controllers/AuthentiationController.cs
@@ -5,22 +5,77 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Activity_Scheduler.Core.Models;
+using Activity_Scheduler.Application.Services.Interfaces;
+using Activity_Scheduler.Core.ViewModels;
 
 namespace Activity_Scheduler.MVC.Controllers
 {
-    [Route("[controller]")]
-    public class AuthentiationController : Controller
+    public class AuthenticationController : Controller
     {
-        private readonly ILogger<AuthentiationController> _logger;
+        private readonly ILogger<AuthenticationController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+        private readonly IAuthentication _authentication;
 
-        public AuthentiationController(ILogger<AuthentiationController> logger)
+        public AuthenticationController(ILogger<AuthenticationController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signinManager, IAuthentication authentication)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signinManager = signinManager;
+            _authentication = authentication;
         }
-
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<ActionResult> Login()
         {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "ActivityScheduler");
+            }
             return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Login(string Email, string Password)
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "ActivityScheduler");
+            }   
+            ApplicationUser user = await _authentication.CheckUser(Email,Password);
+            if (user != null)
+            {
+                await _signinManager.SignInAsync(user,isPersistent: false);
+            }
+            return RedirectToAction("Index","ActivityScheduler");
+        }
+        [HttpGet]
+        public async Task<ActionResult> Register()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "ActivityScheduler");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Register(ApplicationUserViewModel user)
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "ActivityScheduler");
+            }
+            if(!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            ApplicationUser newUser = await _authentication.Register(user);
+            if (user == null)
+            {
+                return null;
+            }
+            _signinManager.SignInAsync(newUser , isPersistent: false);
+            return RedirectToAction("Index","ActivityScheduler");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
