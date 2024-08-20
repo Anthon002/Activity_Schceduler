bfd0234992cf74e4601cdfe058e368d487f93505
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-20 08:13:24 +0100

    Update IActivityScheduler.cs

--- Activity_Scheduler.Application/Services/Interfaces/IActivityScheduler.cs
+++ Activity_Scheduler.Application/Services/Interfaces/IActivityScheduler.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Activity_Scheduler.Core.ViewModels;
 
 namespace Activity_Scheduler.Application.Services.Interfaces
 {
     public interface IActivityScheduler
     {
-        
+        Task<List<ActivityViewModel>> GetActivites(string UserId);
     }
 }
