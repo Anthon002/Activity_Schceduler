6c8d8f228cae12a465c34dfb925d5a10c36118a9
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-20 08:18:44 +0100

    Update IActivitySchedulerRepository.cs

--- Activity_Scheduler.Application/IRepositories/IActivitySchedulerRepository.cs
+++ Activity_Scheduler.Application/IRepositories/IActivitySchedulerRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Activity_Scheduler.Core.ViewModels;
 
 namespace Activity_Scheduler.Application.IRepositories
 {
     public interface IActivitySchedulerRepository
     {
-        
+        Task<List<ActivityViewModel>> GetActivities(string UserId);
     }
 }
