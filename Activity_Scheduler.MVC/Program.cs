f337cf228124eee2c6f7ae11ddc69f4a46cceaf0
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-20 08:05:17 +0100

    Update Program.cs

--- Activity_Scheduler.MVC/Program.cs
+++ Activity_Scheduler.MVC/Program.cs
@@ -5,6 +5,9 @@ using Activity_Scheduler.Core.Models;
 using Activity_Scheduler.Infrastructure.Data;
 using Activity_Scheduler.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 );
 builder.Services.AddScoped<IActivityScheduler,Activity_SchedulerService>();
 builder.Services.AddScoped<IActivitySchedulerRepository,ActivitySchedulerRepository>();
+builder.Services.AddScoped<IAuthentication, AuthenticationService>();
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>(options => {
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireDigit = false;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
@@ -21,11 +34,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -35,6 +50,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authentication}/{action=Register}/{id?}");
 
 app.Run();
