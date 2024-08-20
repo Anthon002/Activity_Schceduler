2da73386b9c58e8f0202a347789a2f014680bba2
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-08-20 08:12:15 +0100

    Update Activity.cs

--- Activity_Scheduler.Core/Models/Activity.cs
+++ Activity_Scheduler.Core/Models/Activity.cs
@@ -8,22 +8,11 @@ namespace Activity_Scheduler.Core.Models
 {
     public class Activity
     {
-        [Required]
         public string Id {get ; set; }
-        [Required]
         public string Title {get; set;}
-        [Required]
         public string Description {get; set;}
-        [Required]
-        [DataType(DataType.DateTime)]
-        public DateTime Created {get; set;}
-        [Required]
-        [DataType(DataType.DateTime)]
         public DateTime StartDate {get;set;}
-        [Required]
-        [DataType(DataType.DateTime)]
         public DateTime EndDate{get;set;}
-        [Required]
         public string UserId{get;set;}
     }
 }
